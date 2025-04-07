b1d4e93097300bfad0624194837bfa7b71b521a0
jacquelineroballo@gmail.com <jacquelineroballo@gmail.com>
2025-04-07 16:08:56 -0300

    sub-hero fixes

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -395,17 +395,11 @@ const Home = () => {
 					</motion.div>
 				</div>
 
-				{/* Wave bottom divider */}
-				<div className='absolute bottom-0 left-0 w-full overflow-hidden'>
-					<svg
-						className='relative block w-full h-[50px] fill-white dark:fill-gray-900'
-						xmlns='http://www.w3.org/2000/svg'
-						viewBox='0 0 1200 120'
-						preserveAspectRatio='none'
-					>
-						<path d='M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V120H0V95.8C59.71,118.11,144.07,96.17,194.42,85.5,262.57,72.93,252.26,57.35,321.39,56.44Z'></path>
+				{/* <div className='absolute bottom-10 left-0 w-full overflow-hidden'>
+					<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1440 320'>
+						<path fill='#ffffff' fill-opacity='1' d='M0,224L1440,64L1440,320L0,320Z'></path>
 					</svg>
-				</div>
+				</div> */}
 			</section>
 
 			<ProjectsSection />
